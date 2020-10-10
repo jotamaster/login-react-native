a16eb42b7ee2e10e51349b9c2c4655295006e2e0
jotamaster <j3anhernandez@gmail.com>
2020-10-10 16:42:04 -0300

    add font icons

--- navigations/Navigator.js
+++ navigations/Navigator.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //navigation
 import {  createSwitchNavigator, createAppContainer } from 'react-navigation';
@@ -35,14 +36,31 @@ export default function Navigator() {
       Pedidos: AppStack,
       Clientes: ClientsScreen,
       },{
-      defaultNavigationOptions: ({ navigation }) => ({
-        
-      }),
-      tabBarOptions: {
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      },
-    });
+        defaultNavigationOptions: ({ navigation }) => ({
+          tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let IconComponent = Ionicons;
+            let iconName;
+            if (routeName === 'Clientes') {
+              iconName = focused
+                ? 'ios-people'
+                : 'ios-people';
+              // Sometimes we want to add badges to some icons.
+              // You can check the implementation below.
+              //IconComponent = HomeIconWithBadge;
+            } else if (routeName === 'Pedidos') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+    
+            // You can return any component that you like here!
+            return <IconComponent name={iconName} size={25} color={tintColor} />;
+          },
+        }),
+        tabBarOptions: {
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        },
+      });
 
     const SwitchNavigator = createSwitchNavigator({
         AuthLoading: AuthLoadingScreen,
